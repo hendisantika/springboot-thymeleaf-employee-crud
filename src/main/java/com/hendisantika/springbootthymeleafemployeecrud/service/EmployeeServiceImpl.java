a718871acbc4d0baecaa29bbe285b5e28c49169f
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-19 22:00:48 +0700

    #10 - Add deleteEmployeeById & findPaginated in method in EmployeeServiceImpl

--- src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeServiceImpl.java
@@ -3,6 +3,10 @@ package com.hendisantika.springbootthymeleafemployeecrud.service;
 import com.hendisantika.springbootthymeleafemployeecrud.entity.Employee;
 import com.hendisantika.springbootthymeleafemployeecrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,6 +47,20 @@ public class EmployeeServiceImpl implements EmployeeService {
             throw new RuntimeException(" Employee not found for id :: " + id);
         }
         return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.employeeRepository.findAll(pageable);
     }
 
 }
