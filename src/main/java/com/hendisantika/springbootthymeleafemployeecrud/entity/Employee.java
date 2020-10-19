fcd89bf3cb24272ee7d213552f84c66a3dedd548
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-19 21:57:30 +0700

    #4 - Add Employee Entity

---
+++ src/main/java/com/hendisantika/springbootthymeleafemployeecrud/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.hendisantika.springbootthymeleafemployeecrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-thymeleaf-employee-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/10/20
+ * Time: 21.56
+ */
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
