cd390e5396c61f20986706bd21349372d73233db
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-19 21:59:53 +0700

    #8 - Add getAllEmployees & saveEmployee method in EmployeeServiceImpl

--- src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeServiceImpl.java
+++ src/main/java/com/hendisantika/springbootthymeleafemployeecrud/service/EmployeeServiceImpl.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springbootthymeleafemployeecrud.service;
 
+import com.hendisantika.springbootthymeleafemployeecrud.entity.Employee;
 import com.hendisantika.springbootthymeleafemployeecrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,5 +21,16 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
 
 }
